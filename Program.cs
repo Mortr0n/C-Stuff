a7f56c9fb58c15f8b29039fc1fae4c411e1e4bb3
Chris G. Morton <chris.g.morton@elmutility.com>
2022-02-25 08:56:22 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using static System.Console;
+namespace FirstCSharp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // string place = "Chris Morton";
+            Random rand = new Random();
+            int randNumber = rand.Next(1,101);
+            // DownThenUp(5, 10);
+            Console.WriteLine($"The return is {WhileReturn(randNumber)}");
+        }
+
+        private static void NewMethod(string place)
+        {
+            WriteLine($"Hello {place}");
+        }
+
+        private static void CountDown(int number)
+        {
+            for(int i = number; i >= 0; i-- ) 
+            {
+                Console.WriteLine($"The number is {i}");
+            }
+        }
+
+        private static void DownThenUp(int firstNumber, int secondNumber) 
+        {
+            for(int i = firstNumber; i >= 0; i--)
+            {
+                Console.WriteLine($"Counting down: {i}");
+            }
+            for(int i = 0; i <= secondNumber; i++) 
+            {
+                Console.WriteLine($"Counting back up: {i}");
+            }
+        }
+
+        
+
+        private static int WhileReturn(int myNumber)
+        {
+            int i = 1;
+            while(i <= myNumber)
+            {
+                Console.WriteLine($"While Loop: {i}");
+                i++;
+            }
+            return myNumber;
+        }
+        
+    }
+}
